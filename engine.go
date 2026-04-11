0bb469e47c0b70d7678acca707aaa0ca0fcc19b6
termux-dewi <165985831+termux-dewi@users.noreply.github.com>
2026-04-11 18:59:33 +0800

    Update engine.go

--- engine.go
+++ engine.go
@@ -1,87 +1,25 @@
-package vpnengine
+package wgserver
 
 import (
-	"log"
-	"net"
-	"sync"
-
-	"golang.zx2c4.com/wireguard/conn"
-	"golang.zx2c4.com/wireguard/device"
-	"golang.zx2c4.com/wireguard/tun"
-	"golang.zx2c4.com/wireguard/tun/netstack"
-	"google.golang.org/grpc"
+    "net"
+    "gvisor.dev/gvisor/pkg/tcpip/stack"
+    "golang.zx2c4.com/wireguard/device"
+    "golang.zx2c4.com/wireguard/tun/netstack"
 )
 
-var (
-	wgDevice  *device.Device
-	isStarted bool
-	mu        sync.Mutex
-)
+// StartServer menjalankan WireGuard server di userspace
+func StartServer(privateKey string, listenPort int, localAddress string) {
+    tun, tnet, err := netstack.CreateNetTUN(
+        []net.IP{net.ParseIP(localAddress)},
+        []net.IP{net.ParseIP("8.8.8.8")},
+        1420)
+    
+    if err != nil {
+        return
+    }
 
-func GenerateKey() string {
-	// Implementasi dummy untuk testing
-	return "PRIVATE_KEY_SAMPEL"
-}
-
-func Stop() {
-	mu.Lock()
-	defer mu.Unlock()
-	if wgDevice != nil {
-		wgDevice.Close()
-		wgDevice = nil
-	}
-	isStarted = false
-	log.Println("VPN Stopped")
-}
-
-// StartClient - Mode Client
-func StartClient(endpoint string, privKey string, pubKey string, localIP string) string {
-	mu.Lock()
-	defer mu.Unlock()
-	if isStarted {
-		return "ALREADY_RUNNING"
-	}
-
-	// Perbaikan: netstack.CreateNetStack (S kapital di Stack)
-	tunDev, _, err := netstack.CreateNetStack(
-		[]net.IP{net.ParseIP(localIP)},
-		[]net.IP{net.ParseIP("8.8.8.8")},
-		1420,
-	)
-	if err != nil {
-		return "TUN_ERROR: " + err.Error()
-	}
-
-	// Perbaikan: NewDevice butuh 3 argumen (tun, bind, logger)
-	logger := device.NewLogger(device.LogLevelError, "vpn:")
-	wgDevice = device.NewDevice(tunDev, conn.NewDefaultBind(), logger)
-
-	isStarted = true
-	return "CLIENT_STARTED"
-}
-
-// StartServer - Mode Server
-func StartServer(port string, privKey string, localIP string) string {
-	mu.Lock()
-	defer mu.Unlock()
-	if isStarted {
-		return "ALREADY_RUNNING"
-	}
-
-	lis, err := net.Listen("tcp", ":"+port)
-	if err != nil {
-		return "BIND_ERROR: " + err.Error()
-	}
-
-	s := grpc.NewServer()
-	// Di sini nanti register service gRPC kamu
-
-	go func() {
-		if err := s.Serve(lis); err != nil {
-			log.Printf("Server failed: %v", err)
-		}
-	}()
-
-	isStarted = true
-	return "SERVER_STARTED"
+    dev := device.NewDevice(tun, device.NewLogger(device.LogLevelError, "wg-go: "))
+    config := "private_key=" + privateKey + "\nlisten_port=" + strconv.Itoa(listenPort)
+    dev.IpcSet(config)
+    dev.Up()
 }
