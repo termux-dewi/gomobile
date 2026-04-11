ec348993bee2a9719aeb17410fffcda15b944756
termux-dewi <165985831+termux-dewi@users.noreply.github.com>
2026-04-11 19:02:13 +0800

    Update engine.go

--- engine.go
+++ engine.go
@@ -1,25 +1,46 @@
-package wgserver
+package vpnengine
 
 import (
-    "net"
-    "gvisor.dev/gvisor/pkg/tcpip/stack"
-    "golang.zx2c4.com/wireguard/device"
-    "golang.zx2c4.com/wireguard/tun/netstack"
+	"net/netip"
+	"strconv"
+
+	"golang.zx2c4.com/wireguard/conn"
+	"golang.zx2c4.com/wireguard/device"
+	"golang.zx2c4.com/wireguard/tun/netstack"
 )
 
-// StartServer menjalankan WireGuard server di userspace
+// StartServer menjalankan WireGuard server di userspace via gVisor netstack
 func StartServer(privateKey string, listenPort int, localAddress string) {
-    tun, tnet, err := netstack.CreateNetTUN(
-        []net.IP{net.ParseIP(localAddress)},
-        []net.IP{net.ParseIP("8.8.8.8")},
-        1420)
-    
-    if err != nil {
-        return
-    }
+	// Konversi string IP ke netip.Addr (API terbaru mewajibkan netip)
+	addr, err := netip.ParseAddr(localAddress)
+	if err != nil {
+		return
+	}
 
-    dev := device.NewDevice(tun, device.NewLogger(device.LogLevelError, "wg-go: "))
-    config := "private_key=" + privateKey + "\nlisten_port=" + strconv.Itoa(listenPort)
-    dev.IpcSet(config)
-    dev.Up()
+	// CreateNetTUN membutuhkan []netip.Addr
+	tun, tnet, err := netstack.CreateNetTUN(
+		[]netip.Addr{addr},
+		[]netip.Addr{netip.MustParseAddr("8.8.8.8")},
+		1420)
+	
+	if err != nil {
+		return
+	}
+
+	// tnet harus digunakan untuk routing atau minimal di-ignore
+	_ = tnet 
+
+	// Device baru sekarang butuh 3 argumen: TUN, Bind, dan Logger
+	dev := device.NewDevice(
+		tun, 
+		conn.NewDefaultBind(), 
+		device.NewLogger(device.LogLevelError, "wg-go: "),
+	)
+
+	// Inisialisasi konfigurasi
+	config := "private_key=" + privateKey + "\nlisten_port=" + strconv.Itoa(listenPort)
+	dev.IpcSet(config)
+	
+	// Jalankan interface
+	dev.Up()
 }
